c811cfd4e69cfeb1834ed7eac7c66613fbea8468
Eugene <git@it-efrem.com>
2021-08-29 15:07:51 +0300

    +- fonts

--- [JS web-version]/src/react/styles/StyleFonts.ts
+++ [JS web-version]/src/react/styles/StyleFonts.ts
@@ -9,24 +9,6 @@ import RobotoWoff2 from './fonts/roboto-regular.woff2';
 // @ts-ignore
 import RobotoTTF from './fonts/roboto-regular.ttf';
 
-// @ts-ignore
-import FontAwesomeRegularEOT from './fonts/fa-regular-400.eot';
-// @ts-ignore
-import FontAwesomeRegularWoff2 from './fonts/fa-regular-400.woff2';
-// @ts-ignore
-import FontAwesomeRegularWoff from './fonts/fa-regular-400.woff';
-// @ts-ignore
-import FontAwesomeRegularTTF from './fonts/fa-regular-400.ttf';
-
-// @ts-ignore
-import FontAwesomeSolidEOT from './fonts/fa-solid-900.eot';
-// @ts-ignore
-import FontAwesomeSolidWoff2 from './fonts/fa-solid-900.woff2';
-// @ts-ignore
-import FontAwesomeSolidWoff from './fonts/fa-solid-900.woff';
-// @ts-ignore
-import FontAwesomeSolidTTF from './fonts/fa-solid-900.ttf';
-
 import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
 import "@fortawesome/fontawesome-free/css/regular.min.css";
 import "@fortawesome/fontawesome-free/css/solid.min.css";
@@ -38,29 +20,5 @@ export const StyleFonts = createGlobalStyle`
         url(${RobotoEOT}) format("embedded-opentype"),
         url(${RobotoWoff}) format("woff"),
         url(${RobotoWoff2}) format("woff2");
-    }
-    
-    @font-face {
-        font-family: "Font Awesome 5 Free";
-        font-style: normal;
-        font-weight: 400;
-        font-display: block;
-        src: url(${FontAwesomeRegularEOT});
-        src: url(${FontAwesomeRegularEOT}) format("embedded-opentype"),
-        url(${FontAwesomeRegularWoff2}) format("woff2"),
-        url(${FontAwesomeRegularWoff}) format("woff"),
-        url(${FontAwesomeRegularTTF}) format("truetype")
-    }
-        
-    @font-face {
-        font-family: "Font Awesome 5 Free";
-        font-style: normal;
-        font-weight: 900;
-        font-display: block;
-        src: url(${FontAwesomeSolidEOT});
-        src: url(${FontAwesomeSolidEOT}) format("embedded-opentype"),
-        url(${FontAwesomeSolidWoff2}) format("woff2"),
-        url(${FontAwesomeSolidWoff}) format("woff"),
-        url(${FontAwesomeSolidTTF}) format("truetype")
     }
 `;
